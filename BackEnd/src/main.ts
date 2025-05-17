f54dac2f955d64ceb1aaf79fc87b644938324052
Mohamed Amine <mohamedamine.soltana@tbs.u-tunis.tn>
2025-05-17 20:49:43 +0100

    Role based ui configured and Profile section added

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: ['http://localhost:5500', 'http://127.0.0.1:5500'], 
   });
@@ -18,6 +20,11 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, documentFactory);
   
+  app.useStaticAssets(
+    join(__dirname, '..', '..', 'FaceRecognition', 'faces'),
+    { prefix: '/faces/' }
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
